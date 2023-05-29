cedc98d3afde9f36a7c738b2b8ce31827b467f55
Piyush Singh <77952712+piyush-codes@users.noreply.github.com>
2023-05-29 21:26:31 +0530

    Time: 63 ms (57.52%) | Memory: 33 MB (88.19%) - LeetSync

--- design-parking-system/design-parking-system.cpp
+++ design-parking-system/design-parking-system.cpp
@@ -1,32 +1,23 @@
 class ParkingSystem {
-    private: int big,med,small;
+    private: int big,medium,small;
 public:
    
    
     ParkingSystem(int big, int medium, int small) {
         this->big=big;
-        this->med=medium;
+        this->medium=medium;
         this->small=small;
     }
     
     bool addCar(int carType) {
-        if(carType==1&&big>=1)
-        {
-            big-=1;
-            return true;
-        }
-       else if(carType==2&&med>=1)
-        {
-            med-=1;
-            return true;
-        }
-        else if(carType==3&&small>=1)
-        {
-            small-=1;
-            return true;
-        }
-        else
-            return false;
+                if(carType == 1 && big == 0 || carType == 2 && medium == 0 || carType == 3 && small == 0)
+          return false;
+
+          if(carType == 1) big--;
+          if(carType == 2) medium--;
+          if(carType == 3) small--;
+
+        return true;
     }
 };
 
