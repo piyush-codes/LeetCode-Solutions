f412c2715add2c40afdac0b84f7930a37569f415
Piyush Singh <77952712+piyush-codes@users.noreply.github.com>
2023-06-11 02:14:40 +0530

    Time: 9 ms (42.28%) | Memory: 7.8 MB (88.88%) - LeetSync

---
+++ counting-bits/counting-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        if(n==0)
+        return {0};
+        vector<int>ans(n+1);
+        ans[0]=0;
+        ans[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            if(i%2==0)
+                ans[i]=ans[i/2];
+            else
+                ans[i]=ans[i/2]+1;
+        }
+        return ans;
+    }
+};
