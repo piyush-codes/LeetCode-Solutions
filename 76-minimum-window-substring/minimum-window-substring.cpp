215226f80bcf4e3c46b9a3633dab1039499a677d
Piyush Singh <77952712+piyush-codes@users.noreply.github.com>
2023-12-10 21:24:31 +0530

    Time: 20 ms (45.67%) | Memory: 8.4 MB (34.97%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        map<char,int>mp;
-        for(auto c:t)mp[c]++;
-        int start=0,end=0,counter=t.size(),minS=0,minL=INT_MAX,size=s.size();
-        while(end<size){
-            if(mp[s[end]]>0)counter--;
-            mp[s[end]]--;
-            end++;
-            while(counter==0)
-            {
-                if(end-start<minL)
+       map<char,int>mp;
+       for(char c:t){
+           mp[c]++;
+       }
+       int start=0,end=0,counter=t.size(),minS=0,minL=INT_MAX;
+       while(end<s.size())
+       {
+           if(mp[s[end++]]-->0)counter--;
+           while(counter==0)
+           {
+               if(end-start<minL)
                 {
-                    minS=start;
                     minL=end-start;
-
+                    minS=start;
                 }
-                mp[s[start]]++;
-                if(mp[s[start]]>0)
-                counter++;
-                start++;
-            }
-        }
-        return minL<INT_MAX?s.substr(minS, minL):"";
+                
+                if(mp[s[start++]]++==0)counter++;
+           }
+       }
+       if(minL<INT_MAX)
+        return s.substr(minS,minL);
+        return "";
     }
 };
