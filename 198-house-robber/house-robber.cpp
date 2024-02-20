3929769cce7df3a1618b7bed0e497219ce952abe
Piyush Singh <77952712+piyush-codes@users.noreply.github.com>
2024-02-20 19:38:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (61.42%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -12,13 +12,15 @@ public:
         return money;    
     }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
+        vector<int>dp(3,0);
         dp[0]=0;
         dp[1]=nums[0];
         for(int i=1;i<nums.size();i++)
         {
-            dp[i+1]=max(nums[i]+dp[i-1],dp[i]);
+            dp[2]=max(nums[i]+dp[0],dp[1]);
+            dp[0]=dp[1];
+            dp[1]=dp[2];
         }
-        return dp[nums.size()];
+        return max(dp[2],dp[1]);
     }
 };
