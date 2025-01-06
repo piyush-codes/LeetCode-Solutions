b67300f226051c75f0916e236b8cfe4a5ed92ee4
Piyush Singh <77952712+piyush-codes@users.noreply.github.com>
2025-01-06 22:55:15 +0530

    Time: 131 ms (7.47%) | Memory: 10.6 MB (9.34%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
+
+    bool oops(vector<char> &str,int i,int j)
+    {
+        cout<<i<<j<<endl;
+        if(i>=str.size()/2)
+            return true;
+        if(str[i]==str[j])
+            return oops(str,i+1,j-1);
+        return false;
+
+    }
     bool isPalindrome(string s) {
-        int i=0,j=s.length()-1;
-        //transform(s.begin(), s.end(), s.begin(), ::tolower);
-        int p;
-        while(i<=j)
-        {
-           
-           if(!isalnum(s[i])){i++; continue;}
-           if(!isalnum(s[j])){j--;continue;}
-            if(tolower(s[i])!=tolower(s[j]))
-                return false;
-            i++;
-            j--;
+        vector<char> str;
+        int j=0;
+        for(auto &i: s){
+            i = tolower(i);
+            if (ispunct(i) or i == ' ') 
+                continue;
+            str.push_back(i);
+            j++;
         }
-        return true;
+        if(j==0)
+            return true;
+            if(j==2)
+            {
+                return str[0]==str[1];
+            }
+        return oops(str, 0,j-1);
     }
 };
