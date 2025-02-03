9be058c4d4834d3b6be2b90d9f41da6832662618
Piyush Singh <77952712+piyush-codes@users.noreply.github.com>
2025-02-03 19:19:47 +0530

    Time: 45 ms (77.39%) | Memory: 29.2 MB (49.95%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -9,6 +9,12 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++)
         {
+            if(nums[i]>0)
+            break;
+        
+        if( i>0 && nums[i]==nums[i-1]){
+            continue;
+        }
             int j=i+1;
             int k=n-1;
             //-4,-1,-1,0,1,2
@@ -16,19 +22,23 @@ public:
             {
                int sum=nums[i]+nums[j]+nums[k];//-4+-1+2=-3
                 if(sum == target){
-                    s.insert({nums[i],nums[j],nums[k]});
-                    j++;
-                    k--;
+                    output.push_back({nums[i],nums[j],nums[k]});
+                    int lo=nums[j];
+                    int ho=nums[k];
+
+                    while(j<k && nums[j]==lo){
+                        j++;
+                    }
+                    while(j<k && nums[k]==ho){
+                        k--;
+                    }
+                    
                 }
                 else if(sum<target)
                     j++;
                 else
                     k--;
             }
-        }
-        for(auto i : s)
-        {
-            output.push_back(i);
         }
         return output;
     }
