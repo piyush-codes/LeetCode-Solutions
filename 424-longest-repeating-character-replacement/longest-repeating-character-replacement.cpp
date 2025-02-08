e219a41d91351e3f3250bfd16b14d18efd43216c
Piyush Singh <77952712+piyush-codes@users.noreply.github.com>
2025-02-08 13:02:53 +0530

    Time: 7 ms (52.75%) | Memory: 11.1 MB (9.16%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-        int characterReplacement(string s, int k) {
-        int ans=0,maxR=0;
-        unordered_map<int,int>mp;
-        for(int i=0;i<s.length();i++)
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> mp;
+        int l=0,r=0,len=0,maxFreq=0,maxLen=0,sz=s.size();
+        for(r=0;r<sz;r++)
         {
-            maxR=max(maxR,++mp[s[i]]);
-            if(ans-maxR<k)
-             ans++;
-             else
-             mp[s[i-ans]]--;
+            char ch=s[r];
+            mp[ch]++;
+            maxFreq=max(maxFreq,mp[ch]);
+            while((r-l+1)-maxFreq>k){
+                mp[s[l]]--;l++;
+            }
+            
+            maxLen=max((r-l+1),maxLen);
         }
-        return ans;
+        return maxLen;
     }
 };
