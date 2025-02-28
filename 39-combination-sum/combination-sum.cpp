e564e93af20e55bde2a9d1d326a13e16050775fc
Piyush Singh <77952712+piyush-codes@users.noreply.github.com>
2025-02-28 20:24:34 +0530

    Time: 2 ms (61.77%) | Memory: 14.3 MB (43.42%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void dfs(int i, vector<int> cur,int target,vector<int>& candidates)
-    {
-        if(target==0)
-        {
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> cur;
+        backtrack(nums, target, cur, 0);
+        return res;
+    }
+
+    void backtrack(vector<int>& nums, int target, vector<int>& cur, int i) {
+        if (target == 0) {
             res.push_back(cur);
             return;
         }
-        if(i==candidates.size()|| target<0)
+        if (target < 0 || i >= nums.size()) {
             return;
+        }
 
-        dfs(i+1,cur,target,candidates);
-        cur.push_back(candidates[i]);
-        dfs(i,cur,target-candidates[i],candidates);
+        cur.push_back(nums[i]);
+        backtrack(nums, target - nums[i], cur, i);
         cur.pop_back();
-
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        dfs(0,{},target,candidates);
-        return res;
+        backtrack(nums, target, cur, i + 1);
     }
 };
